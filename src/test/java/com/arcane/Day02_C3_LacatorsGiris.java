67028aa4294dca8c32f254318205d3792df09598
Hollandali0142 <olcayokur0142@gmail.com>
2022-12-10 10:04:06 +0100

    class3

---
+++ src/test/java/com/arcane/Day02_C3_LacatorsGiris.java
@@ -0,0 +1,52 @@
+package com.arcane;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day02_C3_LacatorsGiris {
+
+  /*  Bir class olustur : LocatorsGiris
+    Main method olustur ve asagidaki gorevi (task) yap.
+    Kullanici "https://www.testyou.in/Login.aspx" gider
+    username textbox, password textbox, and signin button icin locator bul
+    Kullanici adi and password'i gir ve sign in button click et
+    Username : aykut.arcanedata@gmail.com
+    Password : Aykut123!
+    Kullanici adini verify et (USE getText() method to get the text from the page)
+    Home and log out sekmelerinin sayfada gosterildigini dogrula/verify et
+    Sayfada bulunan toplam link sayisini bul
+    Sayfadan log out yapiniz
+    Logged out basarili oldugunu verify edin
+
+ */
+    WebDriver chorome;
+
+    @Before
+        public void setup(){
+
+            WebDriverManager.chromedriver().setup();
+
+            chorome =new ChromeDriver();
+            chorome.manage().window().maximize();
+        }
+
+    @Test
+    public void locatorGiris(){
+
+        chorome.get( "https://www.testyou.in/Login.aspx");
+        //kullanici adini locate ettik ve kullanici adini gonderdik
+      // WebElement emailBox= chorome.findElement(By.id());
+      // emailBox.sendKeys("aykut.arcanedata@gmail.com");
+
+        chorome.
+                findElement();
+
+
+
+    }
+}
