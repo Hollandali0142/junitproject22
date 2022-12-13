94ca793eb69ac3d45917fe518aefc1cf558f8191
Hollandali0142 <olcayokur0142@gmail.com>
2022-12-13 15:09:41 +0100

    x patch

--- src/test/java/com/arcane/Day02_C4_xpath.java
+++ src/test/java/com/arcane/Day02_C4_xpath.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,23 +14,43 @@ public class Day02_C4_xpath {
     // password'u xpath ile locate et
     // Login'i xpath ile locate et
     // log out'u xpath ile locate et
+    // Username : aykut.arcanedata@gmail.com
+    // Password : Aykut123!
 
     WebDriver driver;
     @Before
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+       WebDriverManager.chromedriver().setup();
+       driver=new ChromeDriver();
+       driver.manage().window().maximize();
+
     }
 
     @Test
     public void xpathTest(){
 
+        driver.get("https://www.testyou.in/Login.aspx");
+
+        //email adresini xpath ile locate ettik
+        driver.findElement(By.xpath("//input[@name='ctl00$CPHContainer$txtUserLogin']")).sendKeys("aykut.arcanedata@gmail.com");
+
+        //sifre(password) xpath locate et
+
+        driver.findElement(By.xpath("//input[@id='ctl00_CPHContainer_txtPassword']")).sendKeys("Aykut123!");
+
+        // Login'i xpath ile locate et
+
+        driver.findElement(By.xpath("//input[@name='ctl00$CPHContainer$btnLoginn']")).click();
+
+        // log out'u xpath ile locate et
+
+        driver.findElement(By.xpath("//a[@class='signout']")).click();
+
     }
 
     @After
     public void tearDown(){
-//        driver.close();
+     driver.close();
 
     }
 
