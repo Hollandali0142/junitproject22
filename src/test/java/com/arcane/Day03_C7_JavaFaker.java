abdaee8e0396b3a522d798cacf5454abcdb72766
Hollandali0142 <olcayokur0142@gmail.com>
2022-12-15 23:21:56 +0100

    fake

--- src/test/java/com/arcane/Day03_C7_JavaFaker.java
+++ src/test/java/com/arcane/Day03_C7_JavaFaker.java
@@ -22,6 +22,35 @@ public class Day03_C7_JavaFaker {
 
         System.out.println(fname);
 
+        String lname=faker.name().lastName();
+        System.out.println(lname);
+
+        String fullname=faker.name().fullName();
+        System.out.println(fullname);
+
+        //title
+        System.out.println(faker.name().title());
+
+        //city
+        System.out.println(faker.address().city());
+
+        //state
+        System.out.println(faker.address().state());
+
+        //phone number
+        System.out.println(faker.phoneNumber().cellPhone());
+
+        //zipcode/post code
+
+        System.out.println(faker.address().zipCode());
+
+        // rastgele email
+
+        System.out.println(faker.internet().emailAddress());
+
+        //rastgele rakam
+        System.out.println(faker.number().digits(12));
+
     }
 
 
