42be4140ada6008f65639492346c7fae59625c31
Hollandali0142 <olcayokur0142@gmail.com>
2022-12-19 23:34:33 +0100

    yeni

--- src/test/java/com/arcane/Day04_C10_HesapOlusturma.java
+++ src/test/java/com/arcane/Day04_C10_HesapOlusturma.java
@@ -1,10 +1,25 @@
 package com.arcane;
 
+import com.github.javafaker.Faker;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.concurrent.TimeUnit;
+
 public class Day04_C10_HesapOlusturma {
 
     /*
     Zorunlu tum alanlari doldurun ve Hesap olusturmanin basarili oldugunu verify edin
 1. Create a class HesapOlusturma
+
 2. Go to https://www.automationexercise.com/
 3. Signup/Login link'ine click edin
 4. Isim ve email adresinizi girin ardindan  Signup button"una click edin
@@ -30,4 +45,147 @@ public class Day04_C10_HesapOlusturma {
 24. Ardindan MY ACCOUNT'un sayfada bulundugunuzu verifey edin
      */
 
+        WebDriver driver;
+        Faker faker=new Faker();
+    @Before
+
+    public void setUP() {
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+
+    }
+
+    @Test
+
+    public void hesapolusturma(){
+
+// 2. Go to https://www.automationexercise.com/
+
+        driver.get("https://www.automationexercise.com/");
+
+// 3. Signup/Login link'ine click edin
+       driver.findElement(By.xpath("//a[@href='/login']")).click();
+
+// 4. Isim ve email adresinizi girin ardindan  Signup button"una click edin
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys(faker.name().firstName());
+        driver.findElement(By.xpath("(//input[@name='email'])[2]")).sendKeys(faker.internet().emailAddress());
+
+        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+
+// 5. Text'i Verify et : ENTER ACCOUNT INFORMATION
+
+     Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Enter Account Information']")).isDisplayed());
+
+// 6. Text'i Verify et : ADDRESS INFORMATION
+
+       Assert.assertTrue(driver.findElement(By.xpath("(//h2)[2]")).isDisplayed());
+
+// 7. Text'i Verify et : Title
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Title']")).isDisplayed());
+
+// 8. title'i secin
+
+     WebElement male= driver.findElement(By.xpath("//input[@id='id_gender1']"));
+        if (!male.isSelected()){
+
+            male.click();
+        }
+
+// 9. Name girin (var olani degistir)
+
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys(faker.name().firstName());
+
+//10. Password girin
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(faker.internet().password());
+//11. Date of Birth (dogum tarihi) girin
+        //day
+       WebElement day=driver.findElement(By.xpath("//select[@id='days']"));
+        Select dayDrop=new Select(day);
+            dayDrop.selectByIndex(5);
+        //mand
+       WebElement mand=driver.findElement(By.xpath("//select[@id='months']"));
+       Select mandDrop=new Select(mand);
+       mandDrop.selectByValue("5");
+        //year
+        WebElement year=driver.findElement(By.xpath("//select[@id='years']"));
+        Select yearDrop=new Select(year);
+        yearDrop.selectByIndex(5);
+//12. Sign up for our newsletter! click edin
+       WebElement newsletter= driver.findElement(By.xpath("//*[@for='newsletter']"));
+
+        if(!newsletter.isSelected()){
+
+            newsletter.click();
+        }
+
+//13. Receive special offers from our partners! click edin
+      WebElement  Receive= driver.findElement(By.xpath("//*[@for='optin']"));
+        if (!Receive.isSelected()){
+            Receive.click();
+        }
+
+//14. first name girin
+        driver.findElement(By.xpath("//input[@id='first_name']")).sendKeys(faker.name().firstName());
+
+//15. last name girin
+        driver.findElement(By.xpath("//input[@id='last_name']")).sendKeys(faker.name().lastName());
+
+//16. company girin
+        driver.findElement(By.xpath("//input[@id='company']")).sendKeys(faker.company().name());
+
+//17. Address girin
+
+        driver.findElement(By.xpath("//input[@id='address1']")).sendKeys(faker.address().fullAddress());
+
+//18. Country secin
+
+        WebElement country=driver.findElement(By.xpath("//select[@id='country']"));
+            Select countryDrop=new Select(country);
+            countryDrop.selectByValue("India");
+
+
+//19. State girin
+
+        driver.findElement(By.xpath("//input[@id='state']")).sendKeys(faker.address().state());
+
+//20. City girin
+
+        driver.findElement(By.xpath("//input[@id='city']")).sendKeys(faker.address().state());
+
+
+//21. ZipCode girin
+
+        driver.findElement(By.xpath("//input[@id='zipcode']")).sendKeys(faker.address().zipCode());
+
+//22. mobile phone girin
+
+        driver.findElement(By.xpath("//input[@id='mobile_number']")).sendKeys(faker.phoneNumber().phoneNumber());
+
+
+//23. Create Account'u Click edin
+
+        driver.findElement(By.xpath("(//button[@class='btn btn-default'])[1]")).click();
+
+//24. Ardindan ACCOUNT CREATED!'un sayfada bulundugunuzu verifey edin
+
+       Assert.assertTrue(driver.findElement(By.xpath("//h2[@data-qa='account-created']")).isDisplayed());
+
+
+    }
+
+        @After
+         public void finsh(){
+        driver.close();
+
+        }
+
+
+
+
+
 }
+
+
+
--- src/test/java/com/arcane/Day05_11_Alert.java
+++ src/test/java/com/arcane/Day05_11_Alert.java
@@ -1,2 +1,71 @@
-package com.arcane;public class Day05_11_Alert {
+package com.arcane;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Day05_11_Alert {
+
+    /*
+    Go to https://the-internet.herokuapp.com/javascript_alerts
+    setUp method olustur
+    3 test methodu olustur:
+    acceptAlert() => birinci alert'e tikla, Text'in “I am a JS Alert” oldugunu verify et ve OK'ye tikla , sonra “You successfully clicked an alert” text verify et
+    dismissAlert()=> ikinci alert'e tikla,  Text'in "I am a JS Confirm” oldugunu verify et, cancel'i tikla, sonra “You clicked: Cancel” verify et
+    sendKeysAlert()=> ucuncu alert'e tikla,  Text'in“I am a JS prompt” oldugunu verify et, “Hello Word” Text'i yaz, OK'ye tikla, and “You entered: Hello Word” Text'ini verify et
+ */
+
+    WebDriver driver;
+    @Before
+
+    public void setUP() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+    }
+
+    @Test
+    public void acceptAlert() throws InterruptedException {
+        //   birinci alert'e tikla,
+        driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+        //   Text'in “I am a JS Alert” oldugunu verify et
+        String expected = "I am a JS Alert";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        Thread.sleep(3000);
+        String actualText = driver.switchTo().alert().getText();
+        Assert.assertEquals(expected,actualText);
+        //   ve OK'ye tikla ,
+        driver.switchTo().alert().accept();
+        //   sonra “You successfully clicked an alert” textinin verify et
+
+        String resull=driver.findElement(By.id("result")).getText();
+        Assert.assertEquals("You successfully clicked an alert",resull);
+        System.out.println(driver.findElement(By.id("result")).getText());
+
+
+
+
+    }
+        @Test
+        public void dismissAllert(){
+        //ikinci alert'e tikla
+
+
+        //Text'in "I am a JS Confirm” oldugunu verify et
+
+
+
+        //cancel'i tikla, sonra “You clicked: Cancel” verify et
+
+
+        }
 }
